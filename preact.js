145f78852ee04b791647ad69d460e8be01f749a5
yandong <ereddate@gmail.com>
2016-10-18 10:00:52 +0800

    Update preact.js

--- preact.js
+++ preact.js
@@ -1,11 +1,3 @@
-Object.prototype.toArray = function(fn) {
-	var a = [];
-	for (name in this) {
-		var result = fn(this[name]);
-		result && a.push(this[name]);
-	}
-	return a;
-};
 Array.prototype.del = function(num) {
 	this.splice(num, 1);
 	return this;
@@ -75,10 +67,6 @@ Array.prototype.del = function(num) {
 		return 'null';
 	}
 
-	function _capitalize(val) {
-		return val[0].toUpperCase() + val.substr(1);
-	}
-
 	function _stringify(obj) {
 		if (obj == null) {
 			return 'null';
@@ -89,241 +77,10 @@ Array.prototype.del = function(num) {
 		return _type(obj);
 	}
 
-	function _date(d, pattern) {
-		d = new Date(d);
-		pattern = pattern || 'yyyy-MM-dd';
-		var y = d.getFullYear().toString(),
-			o = {
-				M: d.getMonth() + 1, //month
-				d: d.getDate(), //day
-				h: d.getHours(), //hour
-				m: d.getMinutes(), //minute
-				s: d.getSeconds() //second
-			};
-		pattern = pattern.replace(/(y+)/ig, function(a, b) {
-			return y.substr(4 - Math.min(4, b.length));
-		});
-		for (var i in o) {
-			pattern = pattern.replace(new RegExp('(' + i + '+)', 'g'), function(a, b) {
-				return (o[i] < 10 && b.length > 1) ? '0' + o[i] : o[i];
-			});
-		}
-		return pattern;
-	}
-
-	function _currency(val, symbol) {
-		var places, thousand, decimal;
-		places = 2;
-		symbol = symbol !== undefined ? symbol : "$";
-		thousand = ",";
-		decimal = ".";
-		var number = val,
-			negative = number < 0 ? "-" : "",
-			i = parseInt(number = Math.abs(+number || 0).toFixed(places), 10) + "",
-			j = (j = i.length) > 3 ? j % 3 : 0;
-		return symbol + negative + (j ? i.substr(0, j) + thousand : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousand) + (places ? decimal + Math.abs(number - i).toFixed(places).slice(2) : "");
-	}
-
-	var digitUppercase = function(n, bool) {
-		var fraction = ['角', '分'],
-			digit = [
-				'零', '壹', '贰', '叁', '肆',
-				'伍', '陆', '柒', '捌', '玖'
-			],
-			unit = [
-				['元', '万', '亿'],
-				['', '拾', '佰', '仟']
-			],
-			head = n < 0 ? '欠' : '';
-		n = Math.abs(n);
-		var s = '';
-		for (var i = 0; i < fraction.length; i++) {
-			s += (digit[Math.floor(n * 10 * Math.pow(10, i)) % 10] + (bool ? fraction[i] : '')).replace(/零./, bool ? '' : '零点');
-		}
-		s = s || '整';
-		n = Math.floor(n);
-		for (var i = 0; i < unit[0].length && n > 0; i++) {
-			var p = '';
-			for (var j = 0; j < unit[1].length && n > 0; j++) {
-				p = digit[n % 10] + unit[1][j] + p;
-				n = Math.floor(n / 10);
-			}
-			if (bool) {
-				p = p.replace(/(零.)*零$/, '');
-			} else {
-				p = p.replace(/零./, '零点');
-			}
-			var dw = unit[0][i];
-			if (dw == "元" && !bool) {
-				dw = "点";
-			}
-			s = p.replace(/^$/, '零') + dw + s;
-		}
-		return head + s.replace(/(零.)*零元/, bool ? '元' : '')
-			.replace(/(零.)+/g, bool ? '零' : '零点').replace(/零点$/, "整").replace(/点整$/, "")
-			.replace(/^整$/, bool ? '零元整' : '零整');
-	};
-
-	var getLen = function(str, type) {
-		var str = (str + "").replace(/\r|\n/ig, ""),
-			temp1 = str.replace(/([^\x00-\xff]|[A-Z])/g, "**"),
-			temp2 = temp1.substring(0),
-			x_length = !type ? (temp2.split("\*").length - 1) / 2 + (temp1.replace(/\*/ig, "").length) : temp2.length;
-		return x_length;
-	};
-
-	var textFix = function(name, num) {
-		var max = num ? num : 16;
-		return getLen(name, true) >= max ? _getText(name, max) : name;
-	};
-	var _getText = function(text, max) {
-		var strs = [],
-			n = 0,
-			len = text.length,
-			vtext = "";
-		for (var i = 0; i < len; i++) {
-			vtext = text.substr(i, 1).replace("“", " ").replace("”", " ");
-			if (/([^\x00-\xff]|[A-Z])/.test(vtext)) {
-				n += 2;
-			} else {
-				n += 1;
-			}
-			if (n <= max) {
-				strs.push(vtext);
-			}
-		}
-		return strs.join('');
-	};
-	var _tmplFilterVal = function(val, filterCondition) {
-		if (typeof filterCondition == "function") {
-			return filterCondition(val);
-		} else if (typeof filterCondition == "object") {
-			if ($.isPlainObject(filterCondition)) {
-				for (name in filterCondition) {
-					var oval = filterCondition[name];
-					var oreg = new RegExp(oval, "igm");
-					if (oreg.test(val)) {
-						return val.replace(oreg, "");
-					}
-				}
-			}
-		}
-		var strRegex = new RegExp(filterCondition, "igm");
-		return (val + "").replace(strRegex, "");
-	};
-	var tmplFilter = function($) {
-		//filter|json|limitToCharacter|limitTo|indexOf|lowercase|uppercase|toCNRMB|toCNumber|orderBy|date|currency|empty|passcard|encodeURI|decodeURI|toString|capitalize
-		return {
-			filter: function(val, filterCondition) {
-				return _tmplFilterVal(val, filterCondition);
-			},
-			json: function(val, filterCondition) {
-				return _stringify(val);
-			},
-			limitToCharacter: function(val, filterCondition) {
-				if (is("string", val)) {
-					return textFix(val, parseInt(filterCondition));
-				}
-				return val;
-			},
-			limitTo: function(val, filterCondition) {
-				if (is("array", val)) {
-					var a = [];
-					val.forEach(function(n) {
-						a.push(n);
-					});
-					return _stringify(a.splice(0, parseInt(filterCondition)));
-				} else if (is("string", val)) {
-					return val.substr(0, parseInt(filterCondition)); //textFix(val, parseInt(filterCondition));
-				} else if (is("number", val) && /\./.test(val + "")) {
-					return val.toFixed(filterCondition);
-				} else if (is("number", val)) {
-					var len = (val + "").length;
-					return parseInt((val + "").substr(len - parseInt(filterCondition), filterCondition));
-				}
-				return val;
-			},
-			indexOf: function(val, filterCondition) {
-				var index = -1,
-					i;
-				if (is("array", val)) {
-					for (i = 0; i < val.length; i++) {
-						if (val[i] == filterCondition) {
-							index = i;
-							break;
-						}
-					}
-				}
-				if (is("string", val)) {
-					index = val.indexOf(filterCondition);
-				}
-				return index;
-			},
-			lowercase: function(val, filterCondition) {
-				return val.toLowerCase();
-			},
-			uppercase: function(val, filterCondition) {
-				return val.toUpperCase();
-			},
-			toCNRMB: function(val, filterCondition) {
-				return digitUppercase(parseFloat(val), true);
-			},
-			toCNumber: function(val, filterCondition) {
-				return digitUppercase(parseFloat(val), false);
-			},
-			orderBy: function(val, filterCondition) {
-				if (is("array", val) && /reverse|sort/.test(filterCondition.toLowerCase())) {
-					return val[filterCondition.toLowerCase()]();
-				}
-				return val;
-			},
-			date: function(val, filterCondition) {
-				return _date(val, filterCondition);
-			},
-			currency: function(val, filterCondition) {
-				return _currency(val);
-			},
-			empty: function(val, filterCondition) {
-				return (typeof val == "string" && $.trim(val) == "" || val == null || typeof val == "undefined" || is("object", val) && map.isEmptyObject(val) || is("array", val) && val.length == 0) && filterCondition;
-			},
-			passcard: function(val, filterCondition) {
-				var regex = /(\d{4})(\d{4})(\d{4})(\d{4})(\d{0,})/igm.exec(val);
-				return regex && regex.splice(1, regex.length - 1).join(' ') || val;
-			},
-			encodeURI: function(val, filterCondition) {
-				return encodeURIComponent(val);
-			},
-			decodeURI: function(val, filterCondition) {
-				return decodeURIComponent(val);
-			},
-			toString: function(val, filterCondition) {
-				return _stringify(val);
-			},
-			capitalize: function(val, filterCondition) {
-				switch (filterCondition) {
-					case 0:
-						return _capitalize(val);
-						break;
-					default:
-						var start = filterCondition - 1,
-							end = filterCondition + 1,
-							list = [];
-						val.split('').forEach(function(str, i) {
-							if (i == filterCondition) {
-								list.push(str.toUpperCase())
-							} else {
-								list.push(str);
-							}
-						});
-						return list.length > 0 ? list.join('') : val;
-						break;
-				}
-			}
-		};
-	};
-	tmplFilter = tmplFilter($);
 	$.extend(map, {
 		binds: {},
+		tmplLang: {},
+		tmplFilter: {},
 		repeatTmpl: function(html, data) {
 			var arr = [];
 			var result = html.split("{{ for ");
@@ -346,7 +103,6 @@ Array.prototype.del = function(num) {
 			}
 			return html;
 		},
-		tmplLang: {},
 		tmpl: function(html, data) {
 			if (map.isEmptyObject(data)) return html;
 			map.each(data, function(name, val) {
@@ -355,7 +111,7 @@ Array.prototype.del = function(num) {
 						a = a.replace(new RegExp("{{\\s+(" + name + ")\\s+([^<>,}]+\\s+)*}}"), function(a, b, c) {
 							if (c) {
 								var result = c.split('|')[0].split(' : ');
-								a = a.replace(a, tmplFilter[$.trim(result[0])](val, result[1] && $.trim(result[1]).replace(/[\'\"]/gim, "") || 0));
+								a = a.replace(a, map.tmplFilter[$.trim(result[0])](val, result[1] && $.trim(result[1]).replace(/[\'\"]/gim, "") || 0));
 							} else {
 								a = a.replace(new RegExp("{{\\s+" + name + "\\s+}}", "gim"), val);
 							}
@@ -366,14 +122,28 @@ Array.prototype.del = function(num) {
 				});
 			});
 			map.each(map.tmplLang, function(name, fn) {
-				name != "toArray" && (html = fn(html));
+				html = fn(html);
 			});
 			return html;
 		}
 	});
 	$.extend($, {
+		jsonToArray: function(obj, fn) {
+			var a = [];
+			for (name in obj) {
+				var result = fn(obj[name]);
+				result && a.push(obj[name]);
+			}
+			return a;
+		},
+		stringify: function(obj) {
+			return _stringify(obj);
+		},
+		is: function(str, obj) {
+			return is(str, obj);
+		},
 		tmplFilterExtend: function(filters) {
-			filters && $.extend(tmplFilter, filters);
+			filters && $.extend(map.tmplFilter, filters);
 			return this;
 		},
 		tmplLangExtend: function(langs) {
@@ -392,7 +162,7 @@ Array.prototype.del = function(num) {
 		},
 		isPlainObject: function(obj) {
 			var key, hasOwn = ({}).hasOwnProperty;
-			if (!obj || !is("object", obj)) {
+			if (!obj || !$.is("object", obj)) {
 				return false;
 			}
 			try {
@@ -408,9 +178,9 @@ Array.prototype.del = function(num) {
 			return key === undefined || hasOwn.call(obj, key);
 		},
 		tmpl: function(html, data) {
-			if (is("array", data)) {
+			if ($.is("array", data)) {
 				html = map.repeatTmpl(html, data);
-			} else if (is("object", data)) {
+			} else if ($.is("object", data)) {
 				html = map.tmpl(html, data);
 			}
 			return html;
@@ -453,7 +223,7 @@ Array.prototype.del = function(num) {
 		ready: function(callback) {
 			var a = doc.getElementsByTagName('script'),
 				i, html;
-			a = a.toArray(function(obj) {
+			a = pReact.jsonToArray(a, function(obj) {
 				return map.isElement(obj) ? obj : false;
 			});
 			for (i = 0; i <= a.length; i++) {
@@ -529,7 +299,7 @@ Array.prototype.del = function(num) {
 						if (!$a || $a && $a.length === 0) {
 							var b = doc.getElementsByTagName('script'),
 								i;
-							b = b.toArray(function(obj) {
+							b = pReact.jsonToArray(b, function(obj) {
 								return map.isElement(obj) ? obj : false;
 							});
 							for (i = 0; i <= b.length; i++) {
@@ -572,13 +342,13 @@ Array.prototype.del = function(num) {
 			if (a.children.length > 0) {
 				_.each(a.children, function(i, elem) {
 					_.each(_.binds, function(name, fn) {
-						name != "toArray" && fn(elem, obj);
+						fn(elem, obj);
 					});
 					_.findDom(elem, obj);
 				});
 			} else {
 				_.each(_.binds, function(name, fn) {
-					name != "toArray" && fn(a, obj);
+					fn(a, obj);
 				});
 			}
 		},
