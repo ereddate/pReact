3b20a3e59603502905b1271812353d965ff3c3e5
yandong <ereddate@gmail.com>
2016-12-12 14:14:07 +0800

    更新app，增加对grunt的支持
    更新app，增加对grunt的支持

---
+++ app/Gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+  var pkg = grunt.file.readJSON('package.json');
+  grunt.registerTask("pReact-pjs-concat", "pReactPjsConcat", function() {
+    pkg.configs.forEach(function(f) {
+      var src = f.src,
+        dest = f.dest,
+        ext = ".pjs";
+      var contents = [];
+      src.forEach(function(filepath) {
+        if (grunt.file.exists(filepath + ext)) {
+          contents.push(grunt.file.read(filepath + ext));
+        } else {
+          grunt.log.warn('Source file "' + filepath + ext + '" not found.');
+        }
+      });
+      grunt.file.write(dest + ext, contents.join(''));
+      grunt.log.writeln('file ' + dest + ext + ' created.');
+    });
+  });
+  grunt.registerTask('default', 'pReact-pjs-concat');
+}
