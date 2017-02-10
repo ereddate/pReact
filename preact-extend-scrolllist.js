320d75de06561a863a794c0f1419cf80da1a797f
yandong <ereddate@gmail.com>
2017-02-10 15:44:32 +0800

    更新APP
    更新APP

--- preact-extend-scrolllist.js
+++ preact-extend-scrolllist.js
@@ -19,12 +19,16 @@ pReact && (function(win, pReact) {
 			this.topOffset = options.upElemHeight || this.upElem.height();
 			this.bottomOffset = options.downElemHeight || this.downElem.height();
 			this.maxscroll = this.parent.height();
+			this.unit = options.unit || "rem";
 			var that = this;
 			return this;
 		},
 		refresh: function() {
 			//console.log("refresh")
-			this.maxscroll = this.parent.height();
+			var fz = screen.width / 16;
+			this.topOffset = parseFloat(this.upElem.height())/fz;
+			this.bottomOffset = parseFloat(this.downElem.height())/fz;
+			this.maxscroll = parseFloat(this.parent.height())/fz;
 		},
 		done: function(callback) {
 			var that = this;
@@ -48,20 +52,20 @@ pReact && (function(win, pReact) {
 				var touch = e.changedTouches[0];
 				var deltaY = touch.pageY - this.pointY - that.topOffset;
 				this.currentY = touch.pageY + deltaY + that.topOffset;
-				iscroll.animate(that.content, 0, deltaY, 0);
+				iscroll.animate(that.content, 0, deltaY, 0, that.unit);
 				this.deltaY = deltaY;
 				that.touchMove && that.touchMove.call(that, deltaY);
 			};
 			that.parent[0].ontouchend = function(e) {
 				if (this.currentY > 0) {
 					that.touchEnd ? that.touchEnd.call(that, this.deltaY, function() {
-						iscroll.animate(that.content, 0, 0, 0);
+						iscroll.animate(that.content, 0, 0, 0, that.unit);
 					}, function() {
-						iscroll.animate(that.content, 0, "-" + that.topOffset, 0);
-					}) : iscroll.animate(that.content, 0, "-" + that.topOffset, 0);
+						iscroll.animate(that.content, 0, "-" + that.topOffset, 0, that.unit);
+					}) : iscroll.animate(that.content, 0, "-" + that.topOffset, 0, that.unit);
 					this.currentY = 0;
 				} else if (this.currentY < this.maxscroll) {
-					iscroll.animate(that.content, 0, this.maxscroll, 0);
+					iscroll.animate(that.content, 0, this.maxscroll, 0, that.unit);
 				}
 				that.parent[0].ontouchmove = that.parent[0].ontouchend = null;
 			};
@@ -69,9 +73,9 @@ pReact && (function(win, pReact) {
 			return this;
 		}
 	};
-	iscroll.animate = function(elem, x, y, z) {
+	iscroll.animate = function(elem, x, y, z, unit) {
 		(typeof elem == "string" ? pReact.jq(elem) : elem).css({
-			transform: "translate(" + x + "px, " + y + "px) translateZ(" + z + "px)"
+			transform: "translate(" + x + unit + ", " + y + unit + ") translateZ(" + z + unit + ")"
 		});
 	};
 	iscroll.fn.init.prototype = iscroll.fn;
